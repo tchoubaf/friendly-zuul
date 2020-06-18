a9aec94f229ac56b3f32f7f28d8caf22630e34cb
tchoubaf <tchoubaf@hitriton.com>
2020-06-18 12:33:23 -0500

    created a neww zuul project

---
+++ src/main/java/com/hitriton/friendlyZuulGateway/FriendlyZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hitriton.friendlyZuulGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class FriendlyZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitriton/friendlyZuulGateway/FriendlyZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitriton.friendlyZuulGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
